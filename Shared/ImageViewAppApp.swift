023529d1007b76ec0d649c73473a35df3dddeee8
Leegs <ntcworld@gmail.com>
2022-03-29 04:48:13 +0900

    Initial Commit

---
+++ Shared/ImageViewAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageViewAppApp.swift
+//  Shared
+//
+//  Created by Gwangsoo Lee on 2022/03/29.
+//
+
+import SwiftUI
+
+@main
+struct ImageViewAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
